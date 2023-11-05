b908913c953969f4fca9969bc5120e7004877ec0
aashish_k <aashishkathat00@gmail.com>
2023-11-06 00:02:51 +0530

    build static forms

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ export default function App() {
 				<FormAddFriend />
 				<Button>Add friend</Button>
 			</div>
+			<FormSplitBill />
 		</div>
 	);
 }
@@ -79,4 +80,27 @@ function FormAddFriend() {
 
 function Button({ children }) {
 	return <button className="button">{children}</button>;
+}
+
+function FormSplitBill() {
+	return (
+		<form className="form-split-bill">
+			<h2>Split a bill with X </h2>
+			<label>Bill Value</label>
+			<input type="text" />
+
+			<label>Your expense</label>
+			<input type="text" />
+
+			<label>X expenses</label>
+			<input type="text" disabled />
+
+			<label>Who is payig the bill ? 💲</label>
+			<select>
+				<option value="user">YOU</option>
+				<option value="friend">X</option>
+			</select>
+			<Button>Split Bill</Button>
+		</form>
+	);
 }
